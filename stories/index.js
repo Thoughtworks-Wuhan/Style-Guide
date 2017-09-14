0be832302f1d904b39eee14029e8d2515f2b911e
benweizhu <benweizhu@gmail.com>
2017-09-14 15:20:44 +0800

    add info addon for display code sample

--- stories/index.js
+++ stories/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withInfo } from '@storybook/addon-info';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('with text', withInfo('doc string about my component')(() => <Button onClick={action('clicked')}>Hello Button</Button>))
+  .add('with some emoji', withInfo('doc string about my component')(() => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>));
